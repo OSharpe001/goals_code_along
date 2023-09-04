d80f62359134fbbce59a8cba91c1582b5d0b1559
O. Sharpe <osharpe001@gmail.com>
2023-09-04 12:13:06 -0400

    change Port to point only to my Render backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
-const Port = process.env.PORT || 5010;
+// const Port = process.env.PORT || 5010;
+const Port = process.env.PORT;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 const colors = require("colors");
