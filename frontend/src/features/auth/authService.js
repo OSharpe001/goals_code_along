16840a1a5e79f9f802239672db94ea14e8dbf3d6
O. Sharpe <osharpe001@gmail.com>
2023-09-05 03:22:17 -0400

    it would seem my time is up and I'm out of tricks

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,38 +1,30 @@
 import axios from "axios";
 
-const BASE_URL = "https://sharpgoalsbackend.onrender.com"
+// const BASE_URL = "https://sharpgoalsbackend.onrender.com"
 const API_URL = "/api/users/";
 
 // REGISTER USER
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData);
 
+    console.log("RESPONSE.DATA: ", response.data)
     if (response.data) {
         localStorage.setItem("user", JSON.stringify(response.data));
     };
 
     return response.data
 };
+
 // LOGIN USER
 const login = async (userData) => {
-    const response = await axios.post(`${BASE_URL}${API_URL}login`, userData);
+    const response = await axios.post(`${API_URL}login`, userData);
 
-    console.log("DATA: ", response.data)
     if (response.data) {
         localStorage.setItem("user", JSON.stringify(response.data));
     };
 
     return response.data
 };
-// const login = async (userData) => {
-    //     const { data } = await axios.post(`${BASE_URL}${API_URL}login`, userData);
-    //     console.log("DATA: ", data)
-    //     // return data;
-    //   if (data) {
-    //         localStorage.setItem("user", JSON.stringify(data));
-    //     };
-    //     return data
-    // };
 
 // LOGOUT USER
 const logout = () => {
