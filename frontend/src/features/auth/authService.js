426458d18dd65af8398727a5f7e32769859790f8
O. Sharpe <osharpe001@gmail.com>
2023-09-05 12:56:10 -0400

    latest attempt to get deployed frontend working

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const BASE_URL = "https://newsharpgoals.onrender.com"
+const BASE_URL = "https://newsharpgoals.onrender.com"
 const API_URL = "/api/users/";
 
 // REGISTER USER
 const register = async (userData) => {
-    const response = await axios.post(API_URL, userData);
+    const response = await axios.post(BASE_URL+API_URL, userData);
 
     console.log("RESPONSE.DATA: ", response.data)
     if (response.data) {
@@ -17,7 +17,7 @@ const register = async (userData) => {
 
 // LOGIN USER
 const login = async (userData) => {
-    const response = await axios.post(`${API_URL}login`, userData);
+    const response = await axios.post(`${BASE_URL}${API_URL}login`, userData);
 
     if (response.data) {
         localStorage.setItem("user", JSON.stringify(response.data));
